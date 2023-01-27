e21e50f5f22455204fb8206d5368b8c6daaba1fa
David Vong <dvong95@gmail.com>
2023-01-27 16:37:52 -0500

    Forgot to put myself as an author.

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -8,6 +8,7 @@ import java.util.*;
  *
  * @author dancye
  * @author Paul Bonenfant Jan 25, 2022 
+ * @author David Vong Jan 27, 2023
  */
 public class CardTrick {
     
@@ -50,6 +51,7 @@ public class CardTrick {
      * A simple method to print out personal information. Follow the instructions to 
      * replace this information with your own.
      * @author Paul Bonenfant Jan 2022
+     * @author David Vong Jan 2023
      * I'm done!
      */
     private static void printInfo() {
